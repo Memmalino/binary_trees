7b8036dfe45a937652d9d127756d08380dd2e42a
memmalino <memmalino@gmail.com>
2023-11-02 05:38:37 +0000

    updated

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,11 +7,11 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree)
-    {
-        return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-    }
-    return (0);
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
 }
 
 
@@ -22,13 +22,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree)
-    {
-        size_t left_height = 0, right_height = 0;
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-        left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-        right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-        return ((left_height > right_height) ? left_height : right_height);
-    }
-    return (0);
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
