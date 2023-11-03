4d478fb711b5624a60dea1b0417e54b25c86e69b
memmalino <memmalino@gmail.com>
2023-11-03 02:13:33 +0000

    updated

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,28 +1,46 @@
 #include "binary_trees.h"
+
 /**
- * binary_trees_ancestor - function that locate the lowest minimum ancestor
- * @first: pointer to the first node
- * @second: pointer to the second node
- * Return: the pointer to the L.M.A
+ * find_lca - Recursive function to find the lowest common ancestor
+ * @node: Current node in traversal
+ * @first: First node to find ancestor for
+ * @second: Second node to find ancestor for
+ *
+ * Return: Lowest common ancestor or NULL if not found
  */
-
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *find_lca(const binary_tree_t *node, const binary_tree_t *first,
+const binary_tree_t *second)
 {
-	binary_tree_t *node_a, *node_b;
+	binary_tree_t *left, *right;
 
-	if (!first || !second)
-	{
+	if (node == NULL)
 		return (NULL);
-	}
-	if (first == second)
-	{
-		return ((binary_tree_t *)first);
-	}
-	node_a = first->parent;
-	node_b = second->parent;
-	if (first == node_a || !node_b || (!node_b->parent && node_a))
-		return (binary_trees_ancestor(first, node_a));
-	else if (node_b == second || !node_a || (!node_a->parent && node_b))
-		return (binary_trees_ancestor(node_b, second));
-	return (binary_trees_ancestor(node_b, node_a));
+
+	if (node == first || node == second)
+		return ((binary_tree_t *)node);
+
+	left = find_lca(node->left, first, second);
+	right = find_lca(node->right, first, second);
+
+	if (left && right)
+		return ((binary_tree_t *)node);
+
+	return (left ? left : right);
 }
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: A pointer to the first node
+ * @second: A pointer to the second node
+ *
+ * Return: A pointer to the lowest common ancestor node, or NULL if none found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
+{
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	return (find_lca(first, first, second));
+}
+
